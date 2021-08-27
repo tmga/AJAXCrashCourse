b2ed0ec060ad33e58a3387e2af76242b678a0591
TMGA <tomasgajda261@gmail.com>
2021-08-27 14:23:21 +0200

    hotovo

--- process.php
+++ process.php
@@ -1,9 +1,26 @@
 <?php
+    //Connect to a database
+    $conn = mysqli_connect('localhost', 'root', '', 'ajaxtest');
+
     echo "Processing...";
 
     //Check for GET variables
-
     if(isset($_GET['name'])){
-        echo 'GET: Your name is '.$_GET['name'];
+        echo 'GET: Your name is '.$_GET['name'];  
+    }
+
+    
+    //Check for POST variables
+    if(isset($_POST['name'])){
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        echo 'POST: Your name is '.$_POST['name'];
+
+        $query = "INSERT INTO users(name) VALUES('$name')";
+
+        if(mysqli_query($conn, $query)){
+            echo " User Added...";
+        } else {
+            echo 'ERROR: '. mysqli_error($conn);
+        }
     }
 ?>
